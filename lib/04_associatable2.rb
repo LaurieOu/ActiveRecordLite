8f6bc00e713adab057f1ff993c0eafac2b73b4e5
laurieou <laurieou@gmail.com>
2016-04-05 12:15:50 -0700

    add has_one_through method

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,35 @@
 require_relative '03_associatable'
 
-# Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      from_table = through_options.table_name
+      from_fkey = through_options.foreign_key
+      from_pkey = through_options.primary_key
+
+      join_table = source_options.table_name
+      join_pkey = source_options.primary_key
+      join_fkey = source_options.foreign_key
+
+      where_id = self.send(from_fkey)
+
+      results = DBConnection.execute(<<-SQL, where_id)
+        SELECT
+          #{join_table}.*
+        FROM
+          #{from_table}
+        JOIN
+          #{join_table} ON #{from_table}.#{join_fkey} = #{join_table}.#{join_pkey}
+        WHERE
+          #{from_table}.#{from_pkey} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
